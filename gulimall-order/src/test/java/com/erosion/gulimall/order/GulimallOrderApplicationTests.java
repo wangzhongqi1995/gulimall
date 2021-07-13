fef98f4fe9ea783e44345a030f265eb82454b4df
Emmonite <Wzq1995119>
2021-07-13 21:03:09 +0800

    初始化谷粒商城

---
+++ gulimall-order/src/test/java/com/erosion/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erosion.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
